895acca2334733193002ef56f949b572087d5d47
taoyu65 <taoyu65@gmail.com>
2016-10-11 14:48:39 -0700

    "cc_admin" back end logging in
    building font page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,32 +31,40 @@ $router->pattern('id', '[0-9]+');
 */
 
 Route::group(['middleware' => 'web'], function () {
-    //Route::auth();
-
-    //Route::get('/add', 'HomeController@add');
-    //Route::get('', 'ItemController@create');
     Route::get('', 'HomeController@welcome');
     Route::get('firstpage', 'ItemController@firstPage');
-    //customs
+    #customs
     Route::get('showcustomwindow', 'CustomController@showWindow');
     Route::post('addcustom', 'CustomController@add');
     Route::get('getcustomname/{id}','CustomController@getCustomName');
-
-    //item
+    #item
     Route::resource('item', 'ItemController');
-
-    //store
+    #store
     Route::resource('store', 'StoreController');
-
-    //cart
+    #cart
     Route::get('showcart', 'CartController@showCustomList');
     Route::post('addcart','CartController@add');
     Route::get('searchCart/{custom?}', 'CartController@search');
-
-    //item's pic upload
+    #item's pic upload
     Route::post('additemupload', 'UploadController@imgupload');
     Route::post('additemdelete', 'UploadController@imgDelete');
 
-    Route::auth();
     Route::get('/home', 'HomeController@index');
+    /*
+    |--------------------------------------------------------------------------
+    | cc_admin
+    |--------------------------------------------------------------------------
+    */
+    //Route::auth();
+    Route::group(array('prefix' => 'cc_admin'), function(){
+        Route::get('login', function(){
+            return view('cc_admin/login');
+        });
+        Route::post('submit', 'UserController@submit');
+    });
+
+    Route::group(['middleware' => 'auth'], function () {
+        #login
+    });
 });
+
---
+++ resources/views/cc_admin/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>	
+<head>
+<title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+
+<link href='{{url("cc_admin/css/style.css")}}' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<!--SIGN UP-->
+<h1>ccBuy 后台登录</h1>
+<div class="login-form">
+	<div class="close" onclick="location.href = '{{url("/")}}'"> </div>
+	<div class="head-info">
+		<label class="lbl-1"> </label>
+		<label class="lbl-2"> </label>
+		<label class="lbl-3"> </label>
+	</div>
+	<div class="clear"> </div>
+	<div class="avtar">
+		<img src='{{url("cc_admin/images/avtar.png")}}'>
+	</div>
+	<form method="post" action="{{url('cc_admin/submit')}}">
+		{!! csrf_field() !!}
+		<input type="text" class="text" value="Username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Username';}" >
+		<div class="key">
+			<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
+		</div>
+		<div class="signin">
+			<input type="submit" value="登录" >
+		</div>
+	</form>
+</div>
+<div class="copy-rights">
+	<p>Copyright &copy; 2015.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+</div>
+
+</body>
+</html>
